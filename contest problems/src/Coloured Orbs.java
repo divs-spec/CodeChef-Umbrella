af60851029dc2cdfdbea50d10e5d61d3d07e5fd0
divyani <ikrakizoi2607@gmail.com>
2025-09-11 00:02:35 +0530

    Update Coloured Orbs.java

--- contest problems/src/Coloured Orbs.java
+++ contest problems/src/Coloured Orbs.java
@@ -1,18 +1,21 @@
-#include <bits/stdc++.h>
-using namespace std;
+import java.io.*;
+import java.util.*;
 
-int main() {
-    int R, B;
-    cin >> R >> B;
-    
-    int trades = min(R, B);
-    
-    int greens = trades;
-    int remainingR = R - trades;
-    int remainingB = B - trades;
-    
-    int skill = greens * 5 + remainingR * 1 + remainingB * 2;
-    
-    cout << skill << endl;
-    return 0;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int R = sc.nextInt();
+        int B = sc.nextInt();
+
+        int trades = Math.min(R, B);
+
+        int greens = trades;
+        int remainingR = R - trades;
+        int remainingB = B - trades;
+
+        int skill = greens * 5 + remainingR * 1 + remainingB * 2;
+
+        System.out.println(skill);
+    }
 }
